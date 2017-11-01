dd608c3af8d6ce2c3d94e71999d74e556fcfdc28
kraxik <kraxik@gmail.com>
2017-11-01 19:56:52 +0100

    Update ansible_sandbox.sh

--- ansible_sandbox.sh
+++ ansible_sandbox.sh
@@ -6,9 +6,9 @@ echo "Installing Ansible...."; yum install -y ansible &> /dev/null
 
 echo "Adding sandbox hosts to Ansible...."; cat <<EOT >> /etc/ansible/hosts
 [test_group]
-Client1 ansible_ssh_host=192.168.1.117
-Client2 ansible_ssh_host=192.168.1.132
-Client3 ansible_ssh_host=192.168.1.119
+blastoise ansible_ssh_host=192.168.1.117
+charizard ansible_ssh_host=192.168.1.125
+venusaur ansible_ssh_host=192.168.1.148
 EOT
 
 echo "Disable fingerprints ssh checking...."; sed -i 's/#host_key_checking = False/host_key_checking = False/g' /etc/ansible/ansible.cfg &> /dev/null
